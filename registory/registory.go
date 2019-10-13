0b976a0cf82642d1929ba7e0bb5cc9684ac2c548
koooyooo <jab.cross.hook@gmail.com>
2019-10-13 12:01:23 +0900

    update registory

--- registory/registory.go
+++ registory/registory.go
@@ -1,10 +1,6 @@
 package registory
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-
 	"github.com/koooyooo/soi-go/model"
 )
 
@@ -15,36 +11,4 @@ type Registory interface {
 
 func NewRegistory() Registory {
 	return localRegistroy{}
-}
-
-type localRegistroy struct{}
-
-func (l localRegistroy) Load() (*model.SoiCup, error) {
-	s := model.SoiCup{}
-	b, err := ioutil.ReadFile("sois.json")
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(b, &s)
-	if err != nil {
-		return nil, err
-	}
-	return &s, nil
-}
-
-func (l localRegistroy) Store(s model.SoiCup) error {
-	b, err := json.Marshal(s)
-	if err != nil {
-		return err
-	}
-	var prettyBuff bytes.Buffer
-	err = json.Indent(&prettyBuff, b, "", "  ")
-	if err != nil {
-		return err
-	}
-	err = ioutil.WriteFile("sois.json", prettyBuff.Bytes(), 0666)
-	if err != nil {
-		return err
-	}
-	return nil
 }
