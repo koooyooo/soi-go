538f404fe8d26b7e631665c5c6e4e777057b6cbf
koooyooo <jab.cross.hook@gmail.com>
2020-05-23 01:46:05 +0900

    refactor commons package

---
+++ commons/constant.go
@@ -0,0 +1,19 @@
+package commons
+
+import (
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+)
+
+const (
+	SoisJSONName = "sois.json"
+)
+
+func SoisFilePath() (string, error) {
+	dir, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(dir, SoisJSONName), nil
+}
