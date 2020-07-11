f0e8dd360b814685a66cce5e01298fc8ce2510e0
koooyooo <jab.cross.hook@gmail.com>
2020-07-12 02:36:47 +0900

    update color

--- cmd/prompt/prompt.go
+++ cmd/prompt/prompt.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -12,8 +15,81 @@ import (
 	"github.com/c-bata/go-prompt"
 )
 
+// タイプ中 - 次の階層の候補が表示
+// 決定 - 階層的な候補を決定
+
+// 全ディレクトリの中から有効なディレクトリにマッチしている部分のみ表示
+
+// PreviousTarget は事前の実行結果を記録します
+type PreviousTarget struct {
+	Path     string
+	Suggests []prompt.Suggest
+}
+
+// filter は入力を元にSuggestをフィルタリングします
+func (p PreviousTarget) filter(lastInput string) []prompt.Suggest {
+	var filtered []prompt.Suggest
+	for _, s := range p.Suggests {
+		if strings.HasPrefix(s.Text, lastInput) {
+			filtered = append(filtered, s)
+		}
+	}
+	return filtered
+}
+
+var previousTarget PreviousTarget
+
+func main() {
+	fmt.Println("Please select table.")
+	p := prompt.New(
+		executor,
+		completer,
+		prompt.OptionTitle("soi input"),
+		prompt.OptionPrefix("soi> "),
+		prompt.OptionSelectedSuggestionBGColor(prompt.DarkGray),
+		prompt.OptionSelectedSuggestionTextColor(prompt.White),
+		prompt.OptionSelectedDescriptionBGColor(prompt.DarkGray),
+		prompt.OptionSuggestionBGColor(prompt.LightGray),
+		prompt.OptionSuggestionTextColor(prompt.Black),
+		prompt.OptionDescriptionBGColor(prompt.LightGray),
+	)
+	p.Run()
+}
+
+func executor(in string) {
+	fmt.Printf("EXEC: %s\n", in)
+	if in == "exit" {
+		os.Exit(0)
+	}
+	if strings.HasPrefix(in, "open") {
+		subCmd := strings.TrimPrefix(in, "open ")
+		relPath := strings.ReplaceAll(subCmd, " ", "/")
+		dir, err := soi.SoisDirPath()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullPath := dir + "/" + relPath
+		b, err := ioutil.ReadFile(fullPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var soi SoiData
+		err = json.Unmarshal(b, &soi)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = exec.Command("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", soi.URI).Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("OPENED")
+	}
+}
+
+// completer は補完を実施します
 func completer(d prompt.Document) []prompt.Suggest {
 	//fmt.Println("Cmp", d.TextBeforeCursor())
+
 	s := []prompt.Suggest{
 		{Text: "add", Description: "Add"},
 		{Text: "list", Description: "List"},
@@ -26,9 +102,8 @@ func completer(d prompt.Document) []prompt.Suggest {
 		s = []prompt.Suggest{}
 	}
 	if strings.HasPrefix(textBC, "open") {
-		spaceSepPath := strings.TrimPrefix(textBC, "open ")
-		path := strings.ReplaceAll(spaceSepPath, " ", "/")
-		return readFileInfo(path)
+		input := strings.TrimPrefix(textBC, "open ")
+		return readFileInfo(input)
 	}
 	if strings.HasPrefix(textBC, "list") {
 		s = []prompt.Suggest{}
@@ -45,59 +120,44 @@ func completer(d prompt.Document) []prompt.Suggest {
 	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
 }
 
-type PreviousTarget struct {
-	Path    string
-	Suggest []prompt.Suggest
-}
-
-func (p PreviousTarget) filter(lastInput string) []prompt.Suggest {
-	var filtered []prompt.Suggest
-	for _, s := range p.Suggest {
-		if strings.HasPrefix(s.Text, lastInput) {
-			filtered = append(filtered, s)
-		}
-	}
-	return filtered
-}
-
-var previousTarget PreviousTarget
-
-func readFileInfo(path string) []prompt.Suggest {
-	var s []prompt.Suggest
-	dir, _ := soi.SoisDirPath()
-	if path != "" && path != " " {
-		dir = dir + "/" + path
+// readFileInfo は指定した相対Path(soiRoot以降)を元に Suggestを抽出します
+func readFileInfo(input string) []prompt.Suggest {
+	path := strings.ReplaceAll(input, " ", "/")
+	// 相対パスを元にファイルを抽出
+	rootDir, _ := soi.SoisDirPath()
+	var dir string
+	if path != " " {
+		dir = rootDir + "/" + path
 	}
 	files, _ := ioutil.ReadDir(dir)
-	if len(files) != 0 {
-		for _, f := range files {
-			s = append(s, prompt.Suggest{Text: f.Name(), Description: ""})
+
+	fmt.Println("Input:", path)
+	// ファイルが存在しない場合は直前に保管したSuggestを提示 FIXME: Pathを戻したときの挙動に対応出来ていない
+	if len(files) == 0 {
+		idx := strings.LastIndex(path, "/")
+		if idx != -1 {
+			prevDir := path[0:idx]
+			fmt.Println("PD", prevDir)
 		}
-		previousTarget = PreviousTarget{
-			Path:    dir,
-			Suggest: s,
-		}
-	} else {
+
 		pathElm := strings.Split(path, "/")
 		lastInput := pathElm[len(pathElm)-1]
-		s = previousTarget.filter(lastInput)
+		return previousTarget.filter(lastInput)
+	}
+
+	// ファイルが存在する場合は候補に保存の上、直前のSuggestとして保管
+	var s []prompt.Suggest
+	for _, f := range files {
+		s = append(s, prompt.Suggest{Text: f.Name(), Description: ""})
+	}
+	previousTarget = PreviousTarget{
+		Path:     dir,
+		Suggests: s,
 	}
 	return s
 }
 
-func executor(in string) {
-	fmt.Printf("EXEC: %s\n", in)
-	if in == "exit" {
-		os.Exit(0)
-	}
-}
-
-func main() {
-	fmt.Println("Please select table.")
-	p := prompt.New(executor, completer, prompt.OptionTitle("soi input"), prompt.OptionPrefix("soi> "))
-	p.Run()
-}
-
+// listFiles はsoiRoot配下のファイルを再帰的に追加して Suggestを抽出します
 func listFiles(dir string) []string {
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -114,4 +174,11 @@ func listFiles(dir string) []string {
 	}
 
 	return paths
+}
+
+type SoiData struct {
+	Name    string   `json:"name""`
+	URI     string   `json:"uri"`
+	Tags    []string `json:"tags"`
+	Created string   `json"created"`
 }
--- pkg/fileio/file.go
+++ pkg/fileio/file.go
@@ -10,3 +10,11 @@ func FileExists(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil
 }
+
+func IsDir(path string) (bool, error) {
+	f, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return f.IsDir(), nil
+}
