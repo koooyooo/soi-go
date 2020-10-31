b38b508247995bd2af6f57913931e6ac230ceda8
koooyooo <jab.cross.hook@gmail.com>
2020-10-31 12:30:54 +0900

    add server error handling

--- pkg/srv/server.go
+++ pkg/srv/server.go
@@ -2,13 +2,16 @@ package srv
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func Run() {
 	m := http.NewServeMux()
 	m.Handle("/", http.HandlerFunc(handler))
-	http.ListenAndServe(":8080", m)
+	if err := http.ListenAndServe(":8080", m); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
