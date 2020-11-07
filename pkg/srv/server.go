cdc8d24970b64f4d25d8b43078d376cbdf36ecc9
koooyooo <jab.cross.hook@gmail.com>
2020-11-08 02:31:50 +0900

    make it multitenant based

--- pkg/srv/server.go
+++ pkg/srv/server.go
@@ -16,9 +16,9 @@ import (
 
 func Run() {
 	r := gin.Default()
-	r.GET("/", root)
-	r.GET("/show", showHandlerG)
-	r.POST("/store", storeHandlerG)
+	r.GET("/api/v1/", root)
+	r.GET("/api/v1/:user_id/sois", showHandlerG)
+	r.POST("/api/v1/:user_id/sois", storeHandlerG)
 
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("run server fails: %v", err)
@@ -32,8 +32,9 @@ func root(c *gin.Context) {
 }
 
 func showHandlerG(c *gin.Context) {
+	ctx := createContext(context.Background(), c)
 	repo := repo.NewRepository()
-	sb, err := repo.LoadAll(context.Background())
+	sb, err := repo.LoadAll(ctx)
 	if err != nil {
 		_ = c.AbortWithError(500, err)
 		return
@@ -42,6 +43,7 @@ func showHandlerG(c *gin.Context) {
 }
 
 func storeHandlerG(c *gin.Context) {
+	ctx := createContext(context.Background(), c)
 	b, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
 		_ = c.AbortWithError(500, err)
@@ -53,11 +55,19 @@ func storeHandlerG(c *gin.Context) {
 		return
 	}
 	repo := repo.NewRepository()
-	if err = repo.Store(context.Background(), &s); err != nil {
+	if err = repo.Store(ctx, &s); err != nil {
 		_ = c.AbortWithError(500, err)
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"Store": "OK",
 	})
+}
+
+func createContext(ctx context.Context, gc *gin.Context) context.Context {
+	if userID := gc.Param("user_id"); userID != "" {
+		ctx = context.WithValue(ctx, "user_id", userID)
+	}
+	// TODO 認証情報の埋め込み
+	return ctx
 }
